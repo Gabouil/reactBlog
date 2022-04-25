47d6aa8cdff6dfd5fde6defbbec3a6c2997f10c1
Gabriel Goldbronn <74196777+Gabouil@users.noreply.github.com>
2022-04-25 15:46:56 +0200

    Projet du prof

--- app/login.php
+++ app/login.php
@@ -1,12 +1,44 @@
 <?php
 
-$servername = "db";
-$username = "root";
-$password = "password";
-$database= "data";
+require_once 'headers.php';
+require_once 'Classes/PDOFactory.php';
+require_once 'Classes/User.php';
+require_once 'Classes/CookieHelper.php';
 
-// Création de la connexion
-$db = mysqli_connect($servername, $username, $password, $database);
-if ($db->connect_error) {
-    die("Connexion pas réussi: " . $db->connect_error);
+$username = $_SERVER['PHP_AUTH_USER'] ?? '';
+$password = $_SERVER['PHP_AUTH_PW'] ?? '';
+
+if (!$username || !$password) {
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'username or password parameters missing'
+    ]);
+    exit;
 }
+
+
+$pdo = (new PDOFactory())->getPdo();
+$query = $pdo->prepare('SELECT * FROM `User` WHERE `username` = :username');
+$query->bindValue('username', $username, PDO::PARAM_STR);
+$query->setFetchMode(PDO::FETCH_CLASS, User::class);
+if ($query->execute()) {
+    /** @var User $user */
+    $user = $query->fetch();
+    if ($user && password_verify($password, $user->getPassword())) {
+
+        CookieHelper::setCookie($user->getToken(), $user->getUsername());
+
+        echo json_encode([
+            'status' => 'success',
+            'username' => $user->getUsername(),
+            'token' => $user->getToken()
+        ]);
+        exit;
+    }
+    echo json_encode([
+        'status' => 'error',
+        'message' => 'Wrong credentials'
+    ]);
+}
+
+exit;
